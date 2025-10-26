82e2bcee6a28d213bfc7258aff8ca9ebe73278f5
Samuel Onyebekeh <deemedx0@gmail.com>
2025-10-26 07:45:16 +0000

    add html and css

---
+++ app.js
@@ -0,0 +1,35 @@
+// get all items to be referenced to from html 
+const image = document.getElementById("#image")
+const preview = document.getElementById("#preview")
+const previewList = document.getElementById("#preview-list")
+const item = document.getElementById("#item")
+const price = document.getElementById("#price")
+const quantity = document.getElementById("#quantity")
+const Print = document.getElementById("#checkout")
+const addToList = document.getElementById
+("#addToList")
+console.log(image)
+console.log(preview)
+console.log(previewList)
+console.log(item)
+console.log(price)
+console.log(quantity)
+console.log(Print)
+console.log(addToList)
+
+//name the variable for inventory to be used
+const inventory = [
+    {name: "casamigos",
+     price: "120,000",
+     
+    },
+    {name: "jameson Black",
+     price: "100,000",
+    },
+    {name: "Glenfiddich",
+     price: "800,000",
+    },
+    {name: "heineken",
+     price: "5,000",
+    },
+]
