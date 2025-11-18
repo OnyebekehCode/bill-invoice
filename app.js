e0e0e7efdb70b26e8a91ea33dda81d2e7a104900
onyebekehcode <onyebekehsamuel@gmail.com>
2025-11-18 18:05:07 +0000

    select items

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-// get all items to be referenced to from html 
-const image = document.getElementById("#image")
-const preview = document.getElementById("#preview")
-const previewList = document.getElementById("#preview-list")
-const item = document.getElementById("#item")
-const price = document.getElementById("#price")
-const quantity = document.getElementById("#quantity")
-const Print = document.getElementById("#checkout")
+const image = document.getElementById("image")
+const preview = document.getElementById("preview")
+const previewList = document.getElementById("preview-list")
+const item = document.getElementById("item")
+const price = document.getElementById("price")
+const quantity = document.getElementById("quantity")
+const Print = document.getElementById("checkout")
 const addToList = document.getElementById
-("#addToList")
-console.log(image)
-console.log(preview)
-console.log(previewList)
-console.log(item)
-console.log(price)
-console.log(quantity)
-console.log(Print)
-console.log(addToList)
-
-//name the variable for inventory to be used
+("addToList")
 const inventory = [
     {name: "casamigos",
      price: "120,000",
-     
     },
     {name: "jameson Black",
      price: "100,000",
@@ -33,3 +21,42 @@ const inventory = [
      price: "5,000",
     },
 ]
+
+
+
+    const listItems = () => {
+
+     for (el of inventory){
+    const options = document.createElement("option")
+    console.log(el)
+    options.textContent = el.name
+    item.appendChild(options)
+    item.value = options.textContent
+    console.log(item.value)
+    checkPrice()
+    }
+
+    }
+    
+    
+    const checkPrice = ()=>{
+     for (el of inventory){
+    const option = document.createElement("option")
+    option.textContent = el.price
+    price.appendChild(option)
+    price.value = option.textContent
+     }
+
+    }
+
+const addList = () => {
+    const list = document.createElement("li")
+    list.innerHTML = `<span id="preview-quantity">1</span> 
+     <span id="preview-item">casamigos</span> 
+     <span id="preview-price">120,000</span>`
+     previewList.appendChild(list)
+}
+
+listItems()
+    //console.log(itemPrice)
+    //console.log(item.textContent)
